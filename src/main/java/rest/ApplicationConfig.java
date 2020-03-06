54faefbd2970fc81f3ab99c4e929b121530a811c
Benjamin Paepke <cph-bp133@cphbusiness.dk>
2020-03-06 17:36:47 +0100

    I dont know

--- src/main/java/rest/ApplicationConfig.java
+++ src/main/java/rest/ApplicationConfig.java
@@ -6,7 +6,7 @@ import rest.cors.CorsResponseFilter;
 import java.util.Set;
 import javax.ws.rs.core.Application;
 
-@javax.ws.rs.ApplicationPath("api")
+@javax.ws.rs.ApplicationPath("/api")
 public class ApplicationConfig extends Application {
 
     @Override
